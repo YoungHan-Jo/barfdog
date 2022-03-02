07b637bb8e3d50772eb4c6ad9581e3fb5e800a92
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2022-03-02 18:48:50 +0900

    barfdog

---
+++ barfdog/src/main/java/com/bi/barfdog/BarfdogApplication.java
@@ -0,0 +1,13 @@
+package com.bi.barfdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarfdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarfdogApplication.class, args);
+	}
+
+}
---
+++ barfdog/src/test/java/com/bi/barfdog/BarfdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bi.barfdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarfdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
