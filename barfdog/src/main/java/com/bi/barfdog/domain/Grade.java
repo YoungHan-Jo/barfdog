0cc9759881abadb22129e4f34ff46eee010786a7
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2022-03-04 19:57:02 +0900

    member

---
+++ barfdog/src/main/java/com/bi/barfdog/domain/Grade.java
@@ -0,0 +1,5 @@
+package com.bi.barfdog.domain;
+
+public enum Grade {
+    BRONZE, SILVER, GOLD, PLATINUM, DIAMOND, BARF
+}
--- barfdog/src/main/java/com/bi/barfdog/domain/Member.java
+++ barfdog/src/main/java/com/bi/barfdog/domain/Member.java
@@ -1,9 +1,9 @@
 package com.bi.barfdog.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -11,6 +11,28 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String birthday;
+    private String recCode;
+    private LocalDateTime createDate;
+    private LocalDateTime lastLoginDate;
+    private String recvSms;
+    private String recvEmail;
+    private int rewardPoint;
+
+    @Enumerated(EnumType.STRING)
+    private Grade grade; // [BRONZE, SILVER, GOLD, PLATINUM, DIAMOND, BARF]
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Dog> dogs = new ArrayList<>();
+
 
 
 
