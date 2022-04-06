f4e5cdff28f800a53ab05f3f2c6fb837ba9793f4
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2022-04-06 11:58:20 +0900

    commit

--- barfdog/src/main/java/com/bi/barfdog/domain/rewardPoint/RewardPoint.java
+++ barfdog/src/main/java/com/bi/barfdog/domain/rewardPoint/RewardPoint.java
@@ -1,12 +1,10 @@
 package com.bi.barfdog.domain.rewardPoint;
 
 import com.bi.barfdog.domain.BaseTimeEntity;
+import com.bi.barfdog.domain.member.Member;
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,4 +13,12 @@ public class RewardPoint extends BaseTimeEntity {
     @Id @GeneratedValue
     @Column(name = "reward_point_id")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private RewardPointStatus rewardPointStatus; // [SAVED, USED]
+
+    private RewardPointType type;
 }
---
+++ barfdog/src/main/java/com/bi/barfdog/domain/rewardPoint/RewardPointStatus.java
@@ -0,0 +1,7 @@
+package com.bi.barfdog.domain.rewardPoint;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum RewardPointStatus {
+    SAVED, USED
+}
